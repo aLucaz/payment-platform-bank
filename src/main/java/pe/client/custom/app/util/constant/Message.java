48270e830c7235780b25ed3a968efaf0157cde76
jlucas <jlucas@proveedor.intercorp.com.pe>
2022-11-13 21:12:53 -0500

    Adding properties configuration and comments

--- src/main/java/pe/client/custom/app/util/constant/Message.java
+++ src/main/java/pe/client/custom/app/util/constant/Message.java
@@ -1,6 +1,10 @@
 package pe.client.custom.app.util.constant;
 
 public class Message {
+
+    private Message() {
+    }
+
     public static final String INVALID_REQUEST_DATA_MSG = "Invalid data on the request";
     public static final String UNKNOWN_ERROR_LOG = "Unknown error ocurred";
 }
